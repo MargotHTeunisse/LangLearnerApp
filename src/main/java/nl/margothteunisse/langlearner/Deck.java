fc99935d4d01254e56c247b021b05e23e43c4739
Margot Teunisse <margotteunisse@hotmail.com>
2026-06-22 12:13:19 +0200

    Extract word lists to new Vocabulary class

--- src/main/java/nl/margothteunisse/langlearner/Deck.java
+++ src/main/java/nl/margothteunisse/langlearner/Deck.java
@@ -6,44 +6,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Deck {
-    Map<String, String> cards = new HashMap<>();
+    private final Vocabulary vocab;
+    private int wordIndex;
 
     public Deck(String dataFile) {
-        if (dataFile.equals("empty.txt")) {
-            return;
-        }
-
-        switch (dataFile)
-        {
-            case "cat.txt":
-                cards.put("cat", "kissa");
-                break;
-            case "dog.txt":
-                cards.put("dog", "koira");
-                break;
-            case "bird.txt":
-                cards.put("bird", "lintu");
-                break;
-            case "bear.txt":
-                cards.put("bear", "karhu");
-                break;
-            default:
-                cards.put("", "");
-        }
+        vocab = new Vocabulary(dataFile);
     }
-
+    
+    
     public Card draw() throws DeckEmptyException {
         if (size() == 0) {
             throw new DeckEmptyException();
         }
-        Map.Entry<String, String> entry = cards.entrySet().iterator().next();
-        String front = entry.getKey();
-        String back = entry.getValue();
-        cards.remove(front);
-        return new Card(front, back);
+        Card card = vocab.getCard(wordIndex);
+
+        wordIndex++;
+        return card;
     }
 
     public int size() {
-        return cards.size();
+        return vocab.wordCount() - wordIndex;
     }
 }
---
+++ src/main/java/nl/margothteunisse/langlearner/Vocabulary.java
@@ -0,0 +1,38 @@
+package nl.margothteunisse.langlearner;
+
+import java.util.Map;
+
+class Vocabulary {
+    private final String[][] words;
+
+    Vocabulary(String dataFile) {
+        switch (dataFile)
+        {
+            case "empty.txt":
+                words = new String[][] {};
+                break;
+            case "cat.txt":
+                words = new String[][] {{"cat", "kissa"}};
+                break;
+            case "dog.txt":
+                words = new String[][] {{"dog", "koira"}};
+                break;
+            case "bird.txt":
+                words = new String[][] {{"bird", "lintu"}};
+                break;
+            case "bear.txt":
+                words = new String[][] {{"bear", "karhu"}};
+                break;
+            default:
+                words = new String[][] {{"", ""}};
+        }
+    }
+
+    int wordCount() {
+        return words.length;
+    }
+
+    Card getCard(int wordIndex) {
+        return new Card(words[wordIndex][0], words[wordIndex][1]);
+    }
+}
