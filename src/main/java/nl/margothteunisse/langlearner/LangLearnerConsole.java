211b3174a8f45d3356a33a458468f592485deb4b
Margot Teunisse <margotteunisse@hotmail.com>
2026-07-08 10:28:04 +0200

    Create plain (non-Spring) console version

--- src/main/java/nl/margothteunisse/langlearner/LangLearnerConsole.java
+++ src/main/java/nl/margothteunisse/langlearner/LangLearnerConsole.java
@@ -1,18 +1,34 @@
 package nl.margothteunisse.langlearner;
 
-import nl.margothteunisse.langlearner.controller.ConsoleSession;
 import nl.margothteunisse.langlearner.model.Deck;
 import nl.margothteunisse.langlearner.model.TextVocabulary;
 import org.jspecify.annotations.NonNull;
 
 import java.io.IOException;
+import java.util.Scanner;
+
 
 public class LangLearnerConsole {
     public static void main(String @NonNull [] args) {
-        try (ConsoleSession session = new ConsoleSession(new Deck(new TextVocabulary(args[0])))) {
-            session.run();
+        try  {
+            Deck deck = new Deck(new TextVocabulary(args[0]));
+
+            Scanner scn = new Scanner(System.in);
+            while (deck.draw()) {
+                System.out.println("Translate: " + deck.readCard());
+                String answer = scn.nextLine();
+                if (deck.translateCard(answer)) {
+                    System.out.println("Correct!");
+                }
+                else {
+                    System.out.println("That is incorrect.");
+                }
+            }
+            System.out.println("You have translated all words!");
         } catch (IOException e) {
             System.out.println("Could not find file.");
         }
     }
 }
+
+
