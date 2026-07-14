030123b40bd5ac35f8bc1fa6ada4a2b3e55d76e6
Margot Teunisse <margotteunisse@hotmail.com>
2026-07-14 18:55:13 +0200

    Separate CommandLineRunner from main class for console

--- src/main/java/nl/margothteunisse/langlearner/LangLearnerConsole.java
+++ src/main/java/nl/margothteunisse/langlearner/LangLearnerConsole.java
@@ -1,43 +1,14 @@
 package nl.margothteunisse.langlearner;
 
-import nl.margothteunisse.langlearner.model.Card;
-import nl.margothteunisse.langlearner.model.Deck;
-import org.jspecify.annotations.NonNull;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Profile;
-
-import java.util.Scanner;
 
 @SpringBootApplication
 @Profile("console")
-public class LangLearnerConsole  implements CommandLineRunner{
-    private final Deck deck;
+public class LangLearnerConsole {
 
-    public LangLearnerConsole(Deck deck) {
-        this.deck = deck;
-    }
-
-    public static void main(String @NonNull [] args) {
-        ApplicationContext context = SpringApplication.run(LangLearnerConsole.class, args);
-        SpringApplication.exit(context);
-    }
-
-    public void run(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        while (deck.draw()) {
-            Card card = deck.getDrawnCard();
-            System.out.println("Translate: " + card.read());
-            String answer = scn.nextLine();
-            if (card.check(answer)) {
-                System.out.println("Correct!");
-            }
-            else {
-                System.out.println("That is incorrect.");
-            }
-        }
-        System.out.println("You have translated all words!");
+    public static void main(String[] args) {
+        SpringApplication.run(LangLearnerConsole.class).close();
     }
 }
---
+++ src/main/java/nl/margothteunisse/langlearner/api/ConsoleSession.java
@@ -0,0 +1,35 @@
+package nl.margothteunisse.langlearner.api;
+
+import nl.margothteunisse.langlearner.model.Card;
+import nl.margothteunisse.langlearner.model.Deck;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
+
+import java.util.Scanner;
+
+@Controller
+@Profile("console")
+public class ConsoleSession implements CommandLineRunner {
+    Deck deck;
+
+    public ConsoleSession(Deck deck) {
+        this.deck = deck;
+    }
+
+    public void run(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        while (deck.draw()) {
+            Card card = deck.getDrawnCard();
+            System.out.println("Translate: " + card.read());
+            String answer = scn.nextLine();
+            if (card.check(answer)) {
+                System.out.println("Correct!");
+            }
+            else {
+                System.out.println("That is incorrect.");
+            }
+        }
+        System.out.println("You have translated all words!");
+    }
+}
