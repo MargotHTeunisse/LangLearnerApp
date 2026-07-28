ec32f42d9af634ff9d72ef908bb4dc6af3322a15
Margot Teunisse <margotteunisse@hotmail.com>
2026-07-28 15:23:35 +0200

    Remove references to REST to avoid confusion

--- web-services/src/main/java/nl/margothteunisse/langlearner/api/WebAPI.java
+++ web-services/src/main/java/nl/margothteunisse/langlearner/api/WebAPI.java
@@ -4,29 +4,34 @@ import jakarta.servlet.http.HttpSession;
 import nl.margothteunisse.langlearner.model.Card;
 import nl.margothteunisse.langlearner.model.Deck;
 import nl.margothteunisse.langlearner.model.exceptions.CardFlippedException;
+import nl.margothteunisse.langlearner.view.CardView;
 import nl.margothteunisse.langlearner.view.DeckView;
+import nl.margothteunisse.langlearner.view.FeedbackView;
 import org.jspecify.annotations.NonNull;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/session")
-public class RestAPI implements ApplicationContextAware {
+@Controller
+@RequestMapping("/api")
+public class WebAPI implements ApplicationContextAware {
 
     private ApplicationContext applicationContext;
 
     @GetMapping("/fetch-deck")
+    @ResponseBody
     public DeckView fetchDeck() {
         Deck deck = (Deck) applicationContext.getBean("userDeck");
 
         Card card = deck.getDrawnCard();
-        return new DeckView(card.read(), false, card.getFlipped());
+        return new DeckView(card.read(), card.getFlipped(), false);
     }
 
     @PostMapping("/draw-next-card")
+    @ResponseBody
     public DeckView drawNextCard(HttpSession session) {
         Deck deck = (Deck) applicationContext.getBean("userDeck");
 
@@ -37,29 +42,34 @@ public class RestAPI implements ApplicationContextAware {
         }
 
         Card card = deck.getDrawnCard();
-        return new DeckView(card.read(), deckIsDepleted, card.getFlipped());
+        return new DeckView(card.read(), card.getFlipped(), deckIsDepleted);
+    }
+
+    @GetMapping("/submit")
+    @ResponseBody
+    public ResponseEntity<FeedbackView> submitAnswer(@RequestParam String answer) {
+        Deck deck = (Deck) applicationContext.getBean("userDeck");
+        Card card = deck.getDrawnCard();
+        String cardFront = card.read();
+        try {
+            boolean answerIsCorrect = card.check(answer);
+            FeedbackView feedback = new FeedbackView(cardFront, false, answerIsCorrect);
+            return ResponseEntity.ok().body(feedback);
+        } catch (CardFlippedException e) {
+            FeedbackView feedback = new FeedbackView(cardFront, true, false);
+            return ResponseEntity.badRequest().body(feedback);
+        }
     }
 
     @PostMapping("/show-answer")
-    public DeckView showAnswer() {
+    @ResponseBody
+    public CardView showAnswer() {
         Deck deck = (Deck) applicationContext.getBean("userDeck");
 
         Card card = deck.getDrawnCard();
         card.flip();
 
-        return new DeckView(card.read(), false, card.getFlipped());
-    }
-
-    @PostMapping("/submit")
-    public ResponseEntity<Boolean> submitAnswer(@RequestParam String answer) {
-        Deck deck = (Deck) applicationContext.getBean("userDeck");
-        boolean answerIsCorrect;
-        try {
-            answerIsCorrect = deck.getDrawnCard().check(answer);
-            return ResponseEntity.ok().body(answerIsCorrect);
-        } catch (CardFlippedException e) {
-            return ResponseEntity.badRequest().body(false);
-        }
+        return new CardView(card.read(), card.getFlipped());
     }
 
     @Override
