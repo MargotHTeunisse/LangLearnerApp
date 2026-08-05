6bff759be663d210c980924c6895e05238b775fa
Margot Teunisse <margotteunisse@hotmail.com>
2026-08-05 16:42:25 +0200

    Add API tests to simplified version

---
+++ web-services/src/test/java/nl/margothteunisse/langlearner/api/WebAPITest.java
@@ -0,0 +1,110 @@
+package nl.margothteunisse.langlearner.api;
+
+import nl.margothteunisse.langlearner.dto.CardDTO;
+import nl.margothteunisse.langlearner.model.Card;
+import nl.margothteunisse.langlearner.model.Deck;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.mockito.Mockito.when;
+
+@SpringJUnitConfig(WebAPITest.Config.class)
+public class WebAPITest {
+    @Configuration
+    static class Config {
+        @Bean
+        public WebAPI api() {
+            return new WebAPI();
+        }
+    }
+
+    @MockitoBean("userDeck")
+    private Deck userDeck;
+
+    @Autowired
+    private WebAPI api;
+
+    @Test
+    public void testDrawnCardIsRead() {
+        String front = "cat";
+        when(userDeck.getDrawnCard())
+                .thenReturn(new Card(front, ""));
+
+        CardDTO cardDTO = api.drawnCard();
+
+        Assertions.assertEquals(
+                front,
+                cardDTO.visibleWord());
+    }
+
+    @Test
+    public void testAnswerIsCorrectIfMatchesBack() {
+        String back = "kissa";
+        when(userDeck.getDrawnCard())
+                .thenReturn(new Card("", back));
+
+        boolean answerIsCorrect = api.submitAnswer(back).answerIsCorrect();
+
+        Assertions.assertTrue(answerIsCorrect);
+    }
+
+    @Test
+    public void testAnswerIsIncorrectIfDoesNotMatchBack() {
+        String back = "kissa";
+        when(userDeck.getDrawnCard())
+                .thenReturn(new Card("", back));
+
+        boolean answerIsCorrect = api.submitAnswer("something else").answerIsCorrect();
+
+        Assertions.assertFalse(answerIsCorrect);
+    }
+
+    @Test
+    public void testDeckIsDepletedIfDrawReturnsFalse() {
+        when(userDeck.draw())
+                .thenReturn(false);
+        when(userDeck.getDrawnCard())
+                .thenReturn(new Card("", ""));
+        MockHttpSession session = new MockHttpSession();
+
+        boolean deckIsDepleted = api.drawNextCard(session)
+                .deckIsDepleted();
+
+        Assertions.assertTrue(deckIsDepleted);
+    }
+
+    @Test
+    public void testDeckIsNotDepletedIfDrawReturnsFalse() {
+        when(userDeck.draw())
+                .thenReturn(true);
+        when(userDeck.getDrawnCard())
+                .thenReturn(new Card("", ""));
+        MockHttpSession session = new MockHttpSession();
+
+        boolean deckIsDepleted = api.drawNextCard(session)
+                .deckIsDepleted();
+
+        Assertions.assertFalse(deckIsDepleted);
+    }
+
+    @Test
+    public void testSessionIsInvalidatedIfDeckIsDepleted() {
+        when(userDeck.draw())
+                .thenReturn(false);
+        when(userDeck.getDrawnCard())
+                .thenReturn(new Card("", ""));
+        MockHttpSession session = new MockHttpSession();
+
+
+        api.drawNextCard(session);
+
+        Assertions.assertTrue(session.isInvalid());
+    }
+}
+
