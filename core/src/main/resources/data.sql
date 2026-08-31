ee4b2594a9b9b4eface0601e3ba1722ea65a4501
Margot Teunisse <margotteunisse@hotmail.com>
2026-08-31 16:03:26 +0200

    Add extra vocabularies, including Unicode Test multiple target languages in menu Configure separate data for testing

--- core/src/main/resources/data.sql
+++ core/src/main/resources/data.sql
@@ -1,6 +1,9 @@
 INSERT INTO `decks` (deck_id, first_language, second_language)
 VALUES (1, 'EN', 'FI'),
-(2, 'ES', 'FR');
+(2, 'ES', 'FR'),
+(3, 'JP', 'EN'),
+(4, 'FR', 'FI'),
+(5, 'EN', 'ES');
 COMMIT;
 
 INSERT INTO `cards` (first_word, second_word, deck_id)
@@ -9,7 +12,17 @@ INSERT INTO `cards` (first_word, second_word, deck_id)
 ('dog','koira', 1),
 ('bird','lintu', 1),
 ('bear','karhu', 1),
+('friend', 'ystävä', 1),
 ('gato', 'chat', 2),
 ('perro', 'chien', 2),
-('ave', 'oiseau', 2);
+('ave', 'oiseau', 2),
+('猫', 'cat', 3),
+('犬', 'dog', 3),
+('魚', 'fish', 3),
+('鳥', 'bird', 3),
+('loup', 'susi', 4),
+('jour', 'päivä', 4),
+('pain', 'leipä', 4),
+('dog', 'perro', 5),
+('fish', 'pez', 5);
 COMMIT;
